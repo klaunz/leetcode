09f20b7ca58b381d78e082d81cb8ad49cc1e9770
Kinson Lau <klauiwi@gmail.com>
2025-10-26 23:52:51 +1300

    Create curry.js

---
+++ curry.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Function} func
+ * @return {Function}
+ */
+export default function curry(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.call(this, ...args)
+    }
+    return (...arg) => arg === undefined ? curried.apply(this, args) : curried.apply(this, [...args, ...arg])
+  }
+}
